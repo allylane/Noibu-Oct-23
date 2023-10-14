4f0dacbb6626b611946fc8acaf689489edb1feeb
Alistair Lane <alistair.m.lane@gmail.com>
2023-10-13 23:29:21 -0400

    Navlink

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,18 +1,19 @@
 import Link from 'next/link'
 
-export function NavLink({
-  href,
-  children,
-}: {
-  href: string
-  children: React.ReactNode
-}) {
+interface NavLinkProps {
+  href: string;
+  children: React.ReactNode;
+  className?: string;  // Optional className prop
+}
+
+export function NavLink({ href, children, className = "" }: NavLinkProps) {
+  const combinedClass = `inline-block rounded-lg px-2 py-1 text-base text-slate-900 hover:bg-slate-100 hover:text-slate-900 ${className}`;
+
   return (
-    <Link
-      href={href}
-      className="inline-block rounded-lg px-2 py-1 text-base text-slate-900 hover:bg-slate-100 hover:text-slate-900"
-    >
-      {children}
+    <Link href={href}>
+      <span className={combinedClass}>
+        {children}
+      </span>
     </Link>
   )
 }
