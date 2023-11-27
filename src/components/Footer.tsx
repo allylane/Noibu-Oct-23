518316e2dedf4e76fbb62d83f8fd9be0f8480029
Alistair Lane <alistair.m.lane@gmail.com>
2023-11-26 21:07:34 -0500

    Updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -12,14 +12,18 @@ export function Footer() {
         <div className="relative isolate overflow-hidden">
           <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
             <div className="mx-auto max-w-3xl text-center">
-              <h2 className="text-3xl text-white sm:text-4xl text-balance">
-              Increase successful checkouts for your customers start fixing errors today.
+              <h2 className="text-balance text-3xl text-white sm:text-4xl">
+                Increase successful checkouts for your customers start fixing
+                errors today.
               </h2>
               <p className="mx-auto mt-6 max-w-xl text-xl leading-8 text-white">
                 Start your free trial today. No up front payment required.
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
-              <Button className="px-6 py-3 text-xl montrealsemibold" href="/register">
+                <Button
+                  className="montrealsemibold px-6 py-3 text-xl"
+                  href="/register"
+                >
                   Start free trial
                 </Button>
               </div>
@@ -30,7 +34,13 @@ export function Footer() {
             className="absolute left-1/2 top-1/2 -z-10 h-[64rem] w-[64rem] -translate-x-1/2 [mask-image:radial-gradient(closest-side,white,transparent)]"
             aria-hidden="true"
           >
-            <circle cx={512} cy={512} r={512} fill="url(#8d958450-c69f-4251-94bc-4e091a323369)" fillOpacity="0.5" />
+            <circle
+              cx={512}
+              cy={512}
+              r={512}
+              fill="url(#8d958450-c69f-4251-94bc-4e091a323369)"
+              fillOpacity="0.5"
+            />
             <defs>
               <radialGradient id="8d958450-c69f-4251-94bc-4e091a323369">
                 <stop stopColor="#2600FF" />
@@ -39,14 +49,6 @@ export function Footer() {
             </defs>
           </svg>
         </div>
-  )
-}
-
-
-
-
-
-
       </Container>
     </footer>
   )
